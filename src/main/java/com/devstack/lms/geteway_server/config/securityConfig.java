c9ef78fb820b0895c13e6557422c936e94b1078d
salman <salmannisthar123@gmail.com>
2024-06-13 00:19:04 +0530

    keycloak config

---
+++ src/main/java/com/devstack/lms/geteway_server/config/securityConfig.java
@@ -0,0 +1,17 @@
+package com.devstack.lms.geteway_server.config;
+
+
+
+public class securityConfig {
+
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+//        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers("/eureka/**")
+//                        .permitAll()
+//                        .anyExchange()
+//                        .authenticated());
+//        return  serverHttpSecurity.build();
+//
+//    }
+}
